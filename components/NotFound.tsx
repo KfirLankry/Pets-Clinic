5f066018aadc9048ea20eca5889216ba2edebf47
Kfir Lankry <104919134+KfirLankry@users.noreply.github.com>
2023-02-26 09:05:26 +0200

    .

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -5,7 +5,7 @@ export const NotFound = () => {
   return (
     <div className="empty mt-5 mb-5">
       <p className="text-center mt-5 text-xl text-gray">
-        <i className="fa-solid fa-circle-exclamation"></i> Couldn't find a name
+        <i className="fa-solid fa-circle-exclamation"></i> Couldnt find a name
         on the list...
       </p>
       <Image
